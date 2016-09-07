b58254efefdb4bb90ce07bf13ca424f4abaa6cdd
Scott W Simpkins <ssimpkin@umn.edu>
2016-09-07 17:08:42 -0500

    Making it play nice with config file instead of command line args.

--- scripts/visualize_gene_targets.r
+++ scripts/visualize_gene_targets.r
@@ -10,18 +10,29 @@
 # arguments for row/column info tables and the columns
 # that I want to extract from these tables.
 
-library(getopt)
+library(optparse)
+library(yaml)
 library(reshape2)
 library(data.table)
 library(fastcluster)
 library(ctc)
 
-# Source in the similarity metric(s) I will use 
-# NOTE: I am not computing p values
 TARGET_PATH = Sys.getenv('TARGET_PATH')
 source(file.path(TARGET_PATH, 'lib/cosine.r'))
 source(file.path(TARGET_PATH, 'lib/dist_for_clustering.r'))
 source(file.path(TARGET_PATH, 'lib/filenames.r'))
+
+positional_arguments_list = c(CONFIG_FILE = 'yaml-formatted gene-set prediction configuration file.')
+
+option_list = list(make_option('--rand', action = 'store_true', default = FALSE, help = 'Use this flag if you want to perform visualization for the predicted gene-level targets of the resampled profiles')
+                                      )
+final_usage_positional = get_usage_and_positional(positional_arguments_list)
+parser = OptionParser(usage = final_usage_positional, option_list = option_list)
+arguments = parse_args(parser, positional_arguments = length(positional_arguments_list))
+print(arguments)
+arg = arguments$args
+opt = arguments$options
+
 
 spec = rbind(c('dataset_file', 'f', 1, 'character', 'A 4-column, gzipped table, with one column for the row identifiers (query_key), two columns for the column identifiers (screen_name, expt_id), and one column with gene-target prediction scores (score).'),
              c('clustergram_name', 'n', 1, 'character', 'The name of the cdt/atr/gtr files that are exported.'),
@@ -34,20 +45,44 @@ spec = rbind(c('dataset_file', 'f', 1, 'character', 'A 4-column, gzipped table, 
 
 opt = getopt(spec)
 
+# Read in the configuration parameters
+config_f = file(arg[1], 'rt')
+config_params = yaml.load_file(config_f)
+close(config_f)
+
+output_folder = config_params$Required_arguments$output_folder
+
+gene_target_dir = get_gene_target_folder(output_folder)
+if (opt$rand) {
+    gene_target_filename = get_gene_target_prediction_resampled_filename(
+                                gene_target_dir,
+                                config_params$Required_arguments$`per-array_resampling_scheme`,
+                                config_params$Required_arguments$`num_per-array_resampled_profiles`,
+                                config_params$Required_arguments$`per-array_resampling_seed`,
+                                config_params$Required_arguments$cg_gi_similarity_measure
+                                )
+} else {
+    gene_target_filename = get_gene_target_prediction_filename(
+                                                       gene_target_dir,
+                                                       config_params$Required_arguments$cg_gi_similarity_measure
+                                                       )
+}
+
+
 # Load in the dataset
-dataset_file = normalizePath(opt$dataset_file)
+dataset_file = normalizePath(gene_target_filename)
 tab = fread(sprintf('gzip -dc %s', dataset_file), header = TRUE, colClasses = c('character', 'character', 'character', 'numeric'))
 
-# Define output folder
-output_folder = sub('.txt.gz', '', dataset_file)
-dir.create(output_folder)
+# Read in GI info
+gi_info = get_gi_info(config_params$Required_arguments$gi_dataset_name, TARGET_PATH)
+#gi_tab = fread(sprintf('gzip -dc %s', gi_filenames$gi_tab))
+query_info_tab = fread(gi_info$gi_query_tab, header = TRUE, colClasses = 'character')
 
-# Read in GI info and sample tables
-gi_info_tab = fread(opt$GI_info_table, header = TRUE, colClasses = 'character')
-setkey(gi_info_tab, query_key)
-print(gi_info_tab)
+setkey(query_info_tab, query_key)
+print(query_info_tab)
 
-sample_tab = fread(opt$sample_table, header = TRUE, colClasses = 'character')
+# Read in the CG info
+sample_tab = fread(config_params$cg_col_info_table, header = TRUE, colClasses = 'character')
 sample_tab[, condition_key := sprintf('%s_%s', screen_name, expt_id)]
 setkey(sample_tab, condition_key)
 print(sample_tab)
@@ -58,12 +93,12 @@ tab[, condition_key := sprintf('%s_%s', screen_name, expt_id)]
 mat = acast(data = tab, formula = query_key ~ condition_key, value.var = 'score')
 
 # Get the final rownames of the matrix!
-GI_columns = strsplit(opt$GI_columns, ',')[[1]]
+GI_columns = strsplit(config_params$gene_target_prediction_visualization$gi_col_table_vis_columns, ',')[[1]]
 print(GI_columns)
-row_labels = apply(gi_info_tab[dimnames(mat)[[1]]][, GI_columns, with = FALSE], 1, paste, collapse = '_')
+row_labels = apply(query_info_tab[dimnames(mat)[[1]]][, GI_columns, with = FALSE], 1, paste, collapse = '_')
 
 # Get the final colnames of the matrix!
-condition_columns = strsplit(opt$condition_columns, ',')[[1]]
+condition_columns = strsplit(config_params$gene_target_prediction_visualization$cg_col_table_vis_columns, ',')[[1]]
 print(condition_columns)
 col_labels = apply(sample_tab[dimnames(mat)[[2]]][, condition_columns, with = FALSE], 1, paste, collapse = '_')
 
@@ -79,7 +114,9 @@ hclusts <- lapply(dists, hclust, method = 'average')
 names(hclusts) <- names(dists)
 
 # Get the output filename
-near_full_path = file.path(output_folder, opt$clustergram_name)
+cdt_folder = sub('.txt.gz', '', gene_target_filename)
+dir.create(cdt_folder)
+near_full_path = file.path(cdt_folder, basename(cdt_folder))
 
 r2gtr(hr = hclusts$rows, file = sprintf('%s.gtr', near_full_path), distance = 'custom')
 r2atr(hc = hclusts$cols, file = sprintf('%s.atr', near_full_path), distance = 'custom')
